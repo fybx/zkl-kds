ca521e033f6e4434d336b6fd9e8e2f4f3cb11bee
Yigid BALABAN <fyb@fybx.dev>
2024-08-23 12:14:47 +0300

    check description
    + key-derivation/getPairFromPrivate()
    M key-derivation/generateKeypair to use class Key
    + class Key for interop between different zkl modules

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-import { generateKeyPair, generateMnemonic } from "./key-derivation.service.js";
+import {
+  generateKeyPair,
+  generateMnemonic,
+  newGenKeypair
+} from "./key-derivation.js";
 
 (async () => {
-  const mnemonic = await generateMnemonic();
-  console.log(mnemonic);
+  // const mnemonic = await generateMnemonic();
+  // console.log(mnemonic);
+  console.log("start");
+
+  const mnemonic =
+    "digital radio analyst fine casino have mass blood potato hat web capital prefer debate fee differ spray cloud";
 
   const { publicKey, privateKey } = await generateKeyPair(mnemonic);
-  console.log(publicKey, privateKey);
+  console.log(publicKey.keyType, privateKey.keyType);
+  console.log(publicKey.asHexString, privateKey.asHexString);
+  console.log(publicKey.asByteArray, privateKey.asByteArray);
 
   console.log("done");
 })();
