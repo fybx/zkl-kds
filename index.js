0cd7ca7f653385d2b9d913fe9239ea14165a9a49
Yigid BALABAN <fyb@fybx.dev>
2024-09-25 15:53:20 +0300

    bugs on firefox, chromium is unaffected
    we need further investigation, but have no time to do so.

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-import {
-  generateKeyPair,
-  generateMnemonic,
-  newGenKeypair
-} from "./key-derivation.js";
+import { generateKeypair } from "./key-derivation.js";
 
 (async () => {
-  // const mnemonic = await generateMnemonic();
-  // console.log(mnemonic);
   console.log("start");
 
   const mnemonic =
     "digital radio analyst fine casino have mass blood potato hat web capital prefer debate fee differ spray cloud";
 
-  const { publicKey, privateKey } = await generateKeyPair(mnemonic);
+  const { publicKey, privateKey } = await generateKeypair(mnemonic);
   console.log(publicKey.keyType, privateKey.keyType);
   console.log(publicKey.asHexString, privateKey.asHexString);
   console.log(publicKey.asByteArray, privateKey.asByteArray);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
 const path = require("path");
-const { experiments } = require("webpack");
-const { library } = require("webpack");
 const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist")
   },
   plugins: [
     new webpack.IgnorePlugin({
       checkResource(resource) {
         return /.*\/wordlists\/(?!english).*\.json/.test(resource);
-      },
+      }
     }),
+    new TerserPlugin({
+      terserOptions: {
+        mangle: {
+          reserved: [
+            "generateKeypair",
+            "generateMnemonic",
+            "getPairFromPrivate",
+            "Key"
+          ]
+        }
+      }
+    })
   ],
-  mode: "production",
+  mode: "production"
 };
